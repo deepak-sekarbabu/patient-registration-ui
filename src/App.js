ba8a0b82c6a6475b24e6b7464997176f845c57b0
Deepak Sekarbabu <deepakinmail@gmail.com>
2025-05-22 23:11:50 +0200

    feat: Update Router component to include future configuration options

--- src/App.js
+++ src/App.js
@@ -111,8 +111,12 @@ function App() {
   };
 
   return (
-    <Router>
-      {" "}
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <div className="App">
         <Routes>
           <Route
